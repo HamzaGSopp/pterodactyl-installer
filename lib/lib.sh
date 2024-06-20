34f0acad51b014f534c6f1b6ea735056b68eff8a
HamzaGSopp <104453337+HamzaGSopp@users.noreply.github.com>
2024-06-20 18:49:05 +0200

    Update lib.sh

--- lib/lib.sh
+++ lib/lib.sh
@@ -8,7 +8,7 @@ set -e
 #                                                                                    #
 # Copyright (C) 2018 - 2024, Vilhelm Prytz, <vilhelm@prytznet.se>                    #
 #                                                                                    #
-#   This program is free software: you can redistribute it and/or modify             #;p;pm
+#   This program is free software: you can redistribute it and/or modify             #
 #   it under the terms of the GNU General Public License as published by             #
 #   the Free Software Foundation, either version 3 of the License, or                #
 #   (at your option) any later version.                                              #
@@ -49,10 +49,10 @@ export ARCH=""
 export SUPPORTED=false
 
 # download URLs
-export PANEL_DL_URL="https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz"
-export WINGS_DL_BASE_URL="https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_"
+export PANEL_DL_URL="https://github.com/pelican-dev/panel/releases/latest/download/panel.tar.gz"
+export WINGS_DL_BASE_URL="https://github.com/pelican-dev/wings/releases/latest/download/wings_linux_"
 export MARIADB_URL="https://downloads.mariadb.com/MariaDB/mariadb_repo_setup"
-export GITHUB_BASE_URL=${GITHUB_BASE_URL:-"https://raw.githubusercontent.com/pterodactyl-installer/pterodactyl-installer"}
+export GITHUB_BASE_URL=${GITHUB_BASE_URL:-"https://raw.githubusercontent.com/HamzaGSopp/pterodactyl-installer"}
 export GITHUB_URL="$GITHUB_BASE_URL/$GITHUB_SOURCE"
 
 # Colors
